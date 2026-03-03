d160af346204fdd59d7535deec29146d50460bf2
dylanyunuen <dylanyununen@gmail.com>
2026-03-02 22:12:29 -0600

    Create mision1_saludo.py

---
+++ leccion1/mision1_saludo.py
@@ -0,0 +1,6 @@
+#Misión 1: El Robot que Habla
+# (Las líneas que empiezan con # son notas secretas para nosotros, el robot las ignora)
+
+print("¡Hola mundo! Soy Dylan, un nuevo programador con mucho sueños por delante.")
+print("Tengo 9 años y pronto cumpliré 10.")
+print("Mi superpoder favorito es la super velocidad.")
